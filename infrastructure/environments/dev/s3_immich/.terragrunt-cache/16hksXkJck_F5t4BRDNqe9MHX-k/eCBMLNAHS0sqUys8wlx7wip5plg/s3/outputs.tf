13d2e19f563d6f73fb26c6c60d3cce9d63e4f0a3
Sean Choo <choozexian@gmail.com>
2024-08-04 20:37:55 +0800

    feat(infra): working s3 terraform and terragrunt

--- infrastructure/environments/dev/s3_immich/.terragrunt-cache/16hksXkJck_F5t4BRDNqe9MHX-k/eCBMLNAHS0sqUys8wlx7wip5plg/s3/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "s3_bucket_id" {
-  value       = aws_s3_bucket.this.id
-  description = "The ID of the S3 bucket."
-}
-
-output "s3_bucket_arn" {
-  value       = aws_s3_bucket.this.arn
-  description = "The ARN of the S3 bucket."
-}
